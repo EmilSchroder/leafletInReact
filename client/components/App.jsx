ee7f9c5e0d241167ee461d70c24dc3424fc8a867
EmilSchroder <eschro66@gmail.com>
2018-09-19 14:43:37 +1200

    not worth the trouble

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,26 +1,21 @@
-import React from 'react'
-import {Map, TileLayer} from 'react-leaflet'
-  
-  const stamenTonerTiles = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-  const stamenTonerAttr = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+import React from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
+const Tiles = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const Attr =
+  '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
 
-  export default class App extends React.Component {
-    render() {
-
-      return (
+export default class App extends React.Component {
+  render() {
+    return (
       <div>
-        <Map 
-            center={[-41, 174]}
-            zoom={10}>
-            <TileLayer
-                attribution={stamenTonerAttr}
-                url={stamenTonerTiles}
-            />
+        <Map center={[-41, 174]} zoom={10}>
+          <TileLayer attribution={Attr} url={Tiles} />
+          <Marker position={[-41, 174]}>
+            {/* <Popup>Nicenicenice</Popup> */}
+          </Marker>
         </Map>
       </div>
-  )
+    );
+  }
 }
-}
-
-
